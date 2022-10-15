fd1c270b371b796f19e5dfe6f242b0ae5bc3e6a0
mootqns <mqn360@gmail.com>
2022-10-15 11:45:11 -0700

    removing files

--- secret.py
+++
@@ -1,7 +0,0 @@
-# ---------------------
-# GU Hackathon
-# Client ID & Secret File
-# ---------------------
-
-client_ID = "f4fb2481f83d45b6860a85021cd97bcc"
-client_secret = "2fa62d91cd0948b5a01b830adc38ec3f"
