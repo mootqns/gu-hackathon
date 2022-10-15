4bf00c008062ae2305c06c0d5ccbf5be262c7329
[Henry} <[hsfencer812@gmail.com]>
2022-10-15 11:53:17 -0700

    adding to it

--- utils.py
+++ utils.py
@@ -1,12 +1,22 @@
 import secret
+import json
+import requests
+import base64
 
 # ---------------------
 # GU Hackathon
 # Utilities File
 # ---------------------
 
+def get_access_token():
+    message = secret.client_ID + ":" + secret.client_secret
+    message_bytes = message.encode("ascii")
+    base64_bytes = base64.b64encode(message_bytes)
+    encoded_client_details = base64_bytes.decode("ascii")
+    
+    headers = {"Authorization": "Basic " + encoded_client_details}              
+    body = {"grant_type": "client_credentials"}
+    response = requests.post(url=secret.auth_endpoint, headers=headers, data=body)
+    json_object = json.loads(response.text)
+    return json_object["access_token"]
 # function to load data
-def load_data():
-  print(secret.client_ID)
-
-load_data()
