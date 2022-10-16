bd2a09f2d030e37f3185f4246a5aa14ea59f4e2b
[Henry} <[hsfencer812@gmail.com]>
2022-10-15 18:36:16 -0700

    testing ignore still

--- secret.py
+++
@@ -1,5 +0,0 @@
-client_ID = "f4fb2481f83d45b6860a85021cd97bcc"
-client_secret = "2fa62d91cd0948b5a01b830adc38ec3f"
-
-auth_endpoint = "https://accounts.spotify.com/api/token"
-search_API_endpoint = "https://api.spotify.com/v1/search"
